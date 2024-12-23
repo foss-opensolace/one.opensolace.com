7af5f0b348b69333b2d2689248d2751929a763cd
André devkcud Albanese Junior <patommmmm@proton.me>
2024-12-22 23:01:28 -0300

    chore: Revamp of user register DTO

--- internal/api/model/dto/user.go
+++ internal/api/model/dto/user.go
@@ -1,11 +1,11 @@
 package dto
 
 type UserRegister struct {
-	DisplayName     *string `validate:"omitempty,min=3,max=52" json:"display_name"`
-	Username        string  `validate:"required,username" json:"username"`
-	Email           string  `validate:"required,email" json:"email"`
-	Password        string  `validate:"required,password" json:"password"`
-	PasswordConfirm string  `validate:"required" json:"password_confirm"`
+	DisplayName     string `validate:"omitempty,min=3,max=52" json:"display_name"`
+	Username        string `validate:"required,username,min=3,max=24" json:"username"`
+	Email           string `validate:"required,email" json:"email"`
+	Password        string `validate:"required,password" json:"password"`
+	PasswordConfirm string `validate:"required,eqfield=Password" json:"password_confirm"`
 }
 
 type UserLogin struct {
