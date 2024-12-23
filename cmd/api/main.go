c6088364306f5513d406697c53933f62252aa7b0
André devkcud Albanese Junior <patommmmm@proton.me>
2024-12-22 22:04:11 -0300

    feat: Add db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/foss-opensolace/api.opensolace.com/internal/app"
 	"github.com/foss-opensolace/api.opensolace.com/internal/config"
+	"github.com/foss-opensolace/api.opensolace.com/internal/db"
 )
 
 func main() {
 	config.New()
+	db.New()
 	app.New()
 }
---
+++ internal/db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"github.com/foss-opensolace/api.opensolace.com/internal/api/model"
+	"github.com/foss-opensolace/api.opensolace.com/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var Postgres *gorm.DB
+
+func New() {
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: config.DB.ConnectionString}))
+	if err != nil {
+		panic(err)
+	}
+
+	db.Logger = logger.Discard
+
+	if err := migrate(db); err != nil {
+		panic(err)
+	}
+}
+
+func migrate(db *gorm.DB) error {
+	models := []any{
+		model.User{},
+	}
+
+	if err := db.AutoMigrate(models...); err != nil {
+		return err
+	}
+
+	return nil
+}
